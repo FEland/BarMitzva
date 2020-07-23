67976805ddca0fc35584cea085929384aa49edaa
Effie L <Effieland@aol.com>
2020-07-23 15:24:08 -0400

    fixed master a lil

--- src/Master.js
+++ src/Master.js
@@ -4,8 +4,23 @@ export default function master ( ) {
 
     return (
         <div >
+            <h1>Welcome to Samuel's Bar Mitzva Prep Website! </h1>
+            <parsha_2 style={{backgroundColor: 'gray', color: 'orange'}}>
+    1 When you enter the land that the LORD your God is giving you as a heritage, and you possess it and settle in it,
+    <br></br>2 you shall take some of every first fruit of the soil, which you harvest from the land that the LORD your God is giving you, put it in a basket and go to the place where the LORD your God will choose to establish His name.
+    <br></br>3 You shall go to the priest in charge at that time and say to him, “I acknowledge this day before the LORD your God that I have entered the land that the LORD swore to our fathers to assign us.”
+    <br></br>4 The priest shall take the basket from your hand and set it down in front of the altar of the LORD your God.
+    <br></br> 5 You shall then recite as follows before the LORD your God: “My father was a fugitive Aramean. He went down to Egypt with meager numbers and sojourned there; but there he became a great and very populous nation.
+    <br></br> 6 The Egyptians dealt harshly with us and oppressed us; they imposed heavy labor upon us.
+    <br></br>7 We cried to the LORD, the God of our fathers, and the LORD heard our plea and saw our plight, our misery, and our oppression.
+    <br></br>8 The LORD freed us from Egypt by a mighty hand, by an outstretched arm and awesome power, and by signs and portents.
+    <br></br>9 He brought us to this place and gave us this land, a land flowing with milk and honey.
+    <br></br>10 Wherefore I now bring the first fruits of the soil which You, O LORD, have given me.” You shall leave it before the LORD your God and bow low before the LORD your God.
+    <br></br>11 And you shall enjoy, together with the Levite and the stranger in your midst, all the bounty that the LORD your God has bestowed upon you and your household.
+            
+        </parsha_2>
 
-        <parsha_1 style={{backgroundColor: 'orange', color: 'gray', 'font-size': '110%'}}> 
+        <parsha_1 style={{backgroundColor: 'orange', color: 'gray'}}> 
 
             א וְהָיָה֙ כִּֽי־תָב֣וֹא אֶל־הָאָ֔רֶץ אֲשֶׁר֙ יְהוָ֣ה אֱלֹהֶ֔יךָ נֹתֵ֥ן לְךָ֖ נַֽחֲלָ֑ה וִֽירִשְׁתָּ֖הּ וְיָשַׁ֥בְתָּ בָּֽהּ׃
             <div> ב וְלָֽקַחְתָּ֞ מֵֽרֵאשִׁ֣ית ׀ כָּל־פְּרִ֣י הָֽאֲדָמָ֗ה אֲשֶׁ֨ר תָּבִ֧יא מֵֽאַרְצְךָ֛ אֲשֶׁ֨ר יְהוָ֧ה אֱלֹהֶ֛יךָ נֹתֵ֥ן לָ֖ךְ וְשַׂמְתָּ֣ בַטֶּ֑נֶא וְהָֽלַכְתָּ֙ אֶל־הַמָּק֔וֹם אֲשֶׁ֤ר יִבְחַר֙ יְהוָ֣ה אֱלֹהֶ֔יךָ לְשַׁכֵּ֥ן שְׁמ֖וֹ שָֽׁם׃
@@ -22,20 +37,7 @@ export default function master ( ) {
             </div> יא וְשָֽׂמַחְתָּ֣ בְכָל־הַטּ֗וֹב אֲשֶׁ֧ר נָֽתַן־לְךָ֛ יְהוָ֥ה אֱלֹהֶ֖יךָ וּלְבֵיתֶ֑ךָ אַתָּה֙ וְהַלֵּוִ֔י וְהַגֵּ֖ר אֲשֶׁ֥ר בְּקִרְבֶּֽךָ׃
             <br></br>
         </parsha_1>
-        <parsha_2 style={{backgroundColor: 'gray', color: 'orange', 'font-size': '110%'}}>
-    1 When you enter the land that the LORD your God is giving you as a heritage, and you possess it and settle in it,
-    <br></br>2 you shall take some of every first fruit of the soil, which you harvest from the land that the LORD your God is giving you, put it in a basket and go to the place where the LORD your God will choose to establish His name.
-    <br></br>3 You shall go to the priest in charge at that time and say to him, “I acknowledge this day before the LORD your God that I have entered the land that the LORD swore to our fathers to assign us.”
-    <br></br>4 The priest shall take the basket from your hand and set it down in front of the altar of the LORD your God.
-    <br></br> 5 You shall then recite as follows before the LORD your God: “My father was a fugitive Aramean. He went down to Egypt with meager numbers and sojourned there; but there he became a great and very populous nation.
-    <br></br> 6 The Egyptians dealt harshly with us and oppressed us; they imposed heavy labor upon us.
-    <br></br>7 We cried to the LORD, the God of our fathers, and the LORD heard our plea and saw our plight, our misery, and our oppression.
-    <br></br>8 The LORD freed us from Egypt by a mighty hand, by an outstretched arm and awesome power, and by signs and portents.
-    <br></br>9 He brought us to this place and gave us this land, a land flowing with milk and honey.
-    <br></br>10 Wherefore I now bring the first fruits of the soil which You, O LORD, have given me.” You shall leave it before the LORD your God and bow low before the LORD your God.
-    <br></br>11 And you shall enjoy, together with the Levite and the stranger in your midst, all the bounty that the LORD your God has bestowed upon you and your household.
-            
-        </parsha_2>
+       
         </div>
 );
 }
